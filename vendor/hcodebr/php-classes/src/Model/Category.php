b8a0cb7fd219c3203c0b4e76f1246210624a0917
L-MC1 <lucasm.matriz@gmail.com>
2019-07-11 15:35:20 -0300

    continuar...

--- vendor/hcodebr/php-classes/src/Model/Category.php
+++ vendor/hcodebr/php-classes/src/Model/Category.php
@@ -82,6 +82,20 @@ class Category extends Model {
 		}
 	}
 
+	public function getProductsPage($page = 1, $itemsPerPage = 3){
+
+		$sql = new Sql();
+		$sql->select("
+			SELECT SQL_CALC_FOUND_ROWS *
+			FROM tb_products a
+			INNER JOIN tb_productscategories b ON a.idproduct = b.idproduct
+			INNER JOIN tb_categories c ON c.idcategory = b.idcategory
+			WHERE c.idcategory = 3
+			LIMIT 0, 3;
+
+			SELECT FOUND_ROWS() AS nrtotal;
+			");
+	}
 
 	public function addProduct(Product $product){
 
