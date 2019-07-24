f799466d599b2bf2df55eb352d431ff2604c96b2
L-MC1 <lucasm.matriz@gmail.com>
2019-07-24 12:14:16 -0300

    busca dos users no admin

--- admin-users.php
+++ admin-users.php
@@ -4,11 +4,35 @@ use \Hcode\PageAdmin;
 use \Hcode\Model\User;
 
 $app->get("/admin/users", function(){
-		User::verifyLogin();
-		$users = User::listAll();
+
+	User::verifyLogin();
+
+	$search = (isset($_GET['search'])) ? $_GET['search'] : "";
+	$page = (isset($_GET['page'])) ? (int)$_GET['page'] : 1;
+
+	if($search !=''){
+		$pagination = User::getPageSearch($search, $page);
+	}else {
+		$pagination = User::getPage($page);
+	}	
+
+	$pages = [];
+
+	for ($x = 0; $x < $pagination['pages']; $x++){
+		array_push($pages, [
+			'href'=>'/ecommerce/admin/users?'.http_build_query([
+				'page'=>$x+1,
+				'search'=>$search
+		]),
+			'text'=>$x+1
+	]);
+	}
+
 	$page = new PageAdmin();
 	$page->setTpl("users", array(
-			"users"=>$users
+			"users"=>$pagination['data'],
+			"search"=>$search,
+			"pages"=>$pages
 	));
 });
 
@@ -19,16 +43,21 @@ $app->get("/admin/users/create", function(){
 });
 
 $app->get("/admin/users/:iduser/delete", function($iduser){
+
 		User::verifyLogin();
+
 		$user = new User();
 		$user->get((int)$iduser);
 		$user->delete();
+
 		header("Location: /ecommerce/admin/users");
 		exit;
+
 });
 
 $app->get("/admin/users/:iduser", function($iduser){
-		User::verifyLogin();
+
+	User::verifyLogin();
 
 	$user = new User();
 	$user->get((int)$iduser);
@@ -40,22 +69,33 @@ $app->get("/admin/users/:iduser", function($iduser){
 });
 
 $app->post("/admin/users/create", function(){
+
 	User::verifyLogin();
+
 	$user = new User();
+
 	$_POST["inadmin"] = (isset($_POST["inadmin"]))?1:0;
+
 	$user->setData($_POST);
 	$user->save();
+
 	header("Location: /ecommerce/admin/users");
 	exit;
+
 });
 
 $app->post("/admin/users/:iduser", function($iduser){
+
 		User::verifyLogin();
+
 		$user = new User();
+
 		$_POST["inadmin"] = (isset($_POST["inadmin"]))?1:0;
+
 		$user->get((int)$iduser);
 		$user->setData($_POST);
 		$user->update();
+
 		header("Location: /ecommerce/admin/users");
 		exit;
 
